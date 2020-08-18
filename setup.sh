8975ee84f9331aa14b78425408a72562a8111710
Christopher Krogh <chris.krogh@outlook.com>
2020-08-17 21:47:11 -0400

    reading data from google sheet

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# This script uses pip (https://pypi.org/project/pip/) to install dependencies
+
+# Google dependencies https://developers.google.com/sheets/api/quickstart/python
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
