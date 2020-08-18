05dc5a6db725587b291d946f355b102359ecd46d
Christopher Krogh <chris.krogh@outlook.com>
2020-08-17 23:08:46 -0400

    wip xpaths

--- __main__.py
+++ __main__.py
@@ -1,13 +1,19 @@
-from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SPREADSHEET_ID = '10Lo6SFntXxR9VPTIsnDMMy4K0dvDRBE01MHerxT_VH8'
 RANGE_NAME = 'A1:B1'
+
 
 def get_inputs_from_sheet():
     creds = None
@@ -38,5 +44,31 @@ def get_inputs_from_sheet():
 
     return values
 
+
+def message_friends():
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=/Users/christophermohammed/Library/Application Support/Google/Chrome/Default")
+    driver = webdriver.Chrome(executable_path='/Users/christophermohammed/dev/tools/chromedriver', options=options)
+
+    driver.get("https://web.whatsapp.com/")
+    wait = WebDriverWait(driver, 1000)
+
+    values = get_inputs_from_sheet()
+    for row in values:
+        # search for user
+        search_xpath = '//*[@id="side"]/div[1]/div/label/div'
+        search_bar = wait.until(
+            EC.presence_of_element_located((By.XPATH, search_xpath)))
+        search_bar.send_keys(row[0] + Keys.ENTER)
+        time.sleep(1)
+        # select contact at the top of the list
+        # inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
+        # input_box = wait.until(
+        #     EC.presence_of_element_located((By.XPATH, inp_xpath)))
+        # for i in range(100):
+        #     input_box.send_keys(row[1] + Keys.ENTER)
+        #     time.sleep(1)
+
+
 if __name__ == '__main__':
-    get_inputs_from_sheet()
+    message_friends()
--- setup.sh
+++ setup.sh
@@ -2,3 +2,13 @@
 
 # Google dependencies https://developers.google.com/sheets/api/quickstart/python
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+# Browser automation software
+pip install selenium
+
+# Chromium Web Browser (Open source version of chrome browser)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install chromium-browser
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install chromium
+fi
